fc443464b0c7bcc438c19ca615ac95be80c66e06
Shalima Khanum <Shalima.Khanum@octopusinvestments.com>
2023-07-25 15:16:20 +0100

    test

--- classes/Book.js
+++ classes/Book.js
@@ -1,6 +1,23 @@
 // import the Media class:
-
+import "./Media.js";
 // create your Book class:
+
+class Book extends Media {
+    constructor(title, year, genre, author, numPages, rating) {
+        super(title, year, genre)
+        this.author = author;
+        this.numPages = numPages;
+        this.rating = rating;
+    }
+
+    summary() {
+        return `Title: ${this.title}, Author: ${this.author}, Year: ${this.year}, Page Count: ${this.numPages}, Genre: ${this.genre}, Rating: ${this.rating}`
+    }
+
+    static highestRating() {
+
+    }
+}
 
 // don't change below
 module.exports = { Book }
--- classes/Media.js
+++ classes/Media.js
@@ -1,4 +1,24 @@
 // create your Media class:
+class Media {
+    constructor(title, year, genre, totalMediaCount) {
+        this.title = title;
+        this.year = year;
+        this.genre = genre;
+        this.totalMediaCount = totalMediaCount;
+    }
 
-// uncomment below to export it:
+    summary() {
+        return `Title: ${this.title}, Year: ${this.year}, Genre: ${this.genre}`
+    }
+}
 module.exports = { Media }
+
+const book = new Media('The Catcher in the Rye', 1951, 'Fiction');
+Media.totalMediaCount; // 1
+const music = new Media('Abbey Road', 1969, 'Rock');
+Media.totalMediaCount; // 2
+console.log(music.summary()); // "Title: Abbey Road, Year: 1969, Genre: Rock"
+
+
+// // uncomment below to export it:
+// module.exports = { Media }
