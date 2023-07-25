58e88f1fc58e95c7f4d7fd887eecae8384aa4346
shalima.khanum <shalima.khanum@octopusinvestments.com>
2023-07-25 06:14:12 +0100

    complete finish media.js + book.js

--- classes/Media.js
+++ classes/Media.js
@@ -1,10 +1,13 @@
 // create your Media class:
 class Media {
-    constructor(title, year, genre, totalMediaCount) {
+
+    static totalMediaCount = 0;
+
+    constructor(title, year, genre) {
         this.title = title;
         this.year = year;
         this.genre = genre;
-        this.totalMediaCount = totalMediaCount;
+        Media.totalMediaCount++
     }
 
     summary() {
@@ -13,12 +16,4 @@ class Media {
 }
 module.exports = { Media }
 
-const book = new Media('The Catcher in the Rye', 1951, 'Fiction');
-Media.totalMediaCount; // 1
-const music = new Media('Abbey Road', 1969, 'Rock');
-Media.totalMediaCount; // 2
-console.log(music.summary()); // "Title: Abbey Road, Year: 1969, Genre: Rock"
 
-
-// // uncomment below to export it:
-// module.exports = { Media }
